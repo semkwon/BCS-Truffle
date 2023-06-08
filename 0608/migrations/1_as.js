d91d28a26539c66a63066c36e74f21d631f65a93
semkwon <3pkwon@gmail.com>
2023-06-08 18:52:55 +0900

    Thursday Finished

--- 0608/migrations/1_as.js
+++ 0608/migrations/1_as.js
@@ -1,7 +1,7 @@
 const A = artifacts.require("ADD");
-const B = artifacts.require("SUB");
+// const B = artifacts.require("SUB");
 
 module.exports = function (deployer) {
   deployer.deploy(A);
-  deployer.deploy(B);
+  // deployer.deploy(B);
 };
